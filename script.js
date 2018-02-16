41ae1ec3e1ac1d04f28609eb1b1118f164c47c01
Brian Robinson <elderbrianrobinson@gmail.com>
2018-02-15 22:21:24 -0700

    Project and scripts updated

--- script.js
+++ script.js
@@ -8,54 +8,74 @@ var botTurn = 1;
 var personality1 = 0;
 var personality2 = 0;
 var convoState = "";
+var convoList = [];
 
 $("#talk").click(function(e) {
 	e.preventDefault();
-	var value = $("#topic").val();
-	if (value == undefined)
+	var value = '<span style="color: '
+	+ (botTurn == 1 ? "blue" : "red") + ';">'
+	+ $("#topic").val()
+	+ '</span>';
+	if ($("#topic").val().length > 0)
 	{
-		value = $("#chatbox").text();
+		$("#chatbox").html(value);
+		$("#topic").val("");
+		convoState = "";
+		convoList = [];
+		convoList.push(value);
+		if (botTurn == 1) botTurn = 2;
+		else botTurn = 1;
+		return;
 	}
-	var myurl= "http://www.cleverbot.com/getreply?key=CC77xfCzAkGo092Mooho0trqnB"
-		+ "&input=" + $("#chatbox").val
+	var myurl= "http://www.cleverbot.com/getreply?key=KEY"
+		+ "&input=" + $("#chatbox").text()
 		+ "&cs=" + convoState
-		+ "&cb_settings_tweak1" + getPersonality(botTurn, 1)
-		+ "&cb_settings_tweak2" + getPersonality(botTurn, 2)
-		+ "&cb_settings_tweak3" + getPersonality(botTurn, 3);
+		+ "&cb_settings_tweak1" + getPersonality(botTurn, 0)
+		+ "&cb_settings_tweak2" + getPersonality(botTurn, 1)
+		+ "&cb_settings_tweak3" + getPersonality(botTurn, 2);
 	$.ajax({
 	    url : myurl,
 	    dataType : "json",
-	    success : setChat
+	    success : addChat
 	});
 });
 
-$("#new").click(function(e) {
-	e.preventDefault();
-	botTurn = 1;
-	convoState = "";
-	$("#chatbox").text("");
-});
-
-var setChat = function(json){
+var addChat = function(json){
 	console.log(json);
+	if (convoList.push(
+	'<span style="color: '
+	+ (botTurn == 1 ? "blue" : "red") + ';">'
+	+ json.output + '</span>'
+	) > 9)
+	{
+		convoList.shift();
+	}
+	console.log(json.output);
+	var chat = "";
+	for (i = 0; i < convoList.length; i++)
+	{
+		chat += convoList[i] + "</br>";
+	}
+	$("#chatbox").html(chat);
 	convoState = json.cs;
-	$("#chatbox").text(json.output);
 	if (botTurn == 1) botTurn = 2;
 	else botTurn = 1;
-	$("#topic").val = undefined;
 }
 
-var personalityValues = [[30, 50, 70], [70, 50, 70], [30, 80, 70], [30, 20, 70]]
+var personalityValues = [[20, 50, 80], [90, 50, 80], [20, 90, 80], [20, 10, 80]]
 
 var getPersonality = function(botNum, pSetting){
-	if (botNum == 1)
-	{
-		return personalityValues[personality1][pSetting];
-	}
 	if (botNum == 2)
 	{
-		return personalityValues[personality2][pSetting];
+		console.log(personalityValues[parseInt($("#pbot1").prop('selectedIndex'))][pSetting]);
+		return personalityValues[parseInt($("#pbot1").prop('selectedIndex'))][pSetting];
 	}
+	if (botNum == 1)
+	{
+		console.log(personalityValues[parseInt($("#pbot2").prop('selectedIndex'))][pSetting]);
+		return personalityValues[parseInt($("#pbot2").prop('selectedIndex'))][pSetting];
+	}
+	return personalityValues[parseInt($("#pbot1").prop('selectedIndex'))][pSetting];
 }
 
 function drop() {
